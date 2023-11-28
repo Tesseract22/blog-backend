a7036bed79f3335bceda546cfdd290a830bb3820
Tesseract22 <caleb2222@qq.com>
2023-11-28 05:32:55 +0000

    a

--- src/config.zig
+++ src/config.zig
@@ -5,7 +5,7 @@ pub const AuthFile = "auth";
 pub const Auth = "Bearer " ++ @embedFile(AuthFile);
 pub const Port = switch (builtin.mode) {
     .Debug => 3300,
-    else => 80,
+    else => 3000,
 };
 pub const Domain = "tesseract.cat";
 pub const AdminCookieName = "admin-cookie";
