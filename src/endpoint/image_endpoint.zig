e71dd1cf03c7cb8e9b097bfd360804947d8ab7cc
Tesseract22 <caleb2222@qq.com>
2024-05-14 03:57:05 -0500

    update uri decode

--- src/endpoint/image_endpoint.zig
+++ src/endpoint/image_endpoint.zig
@@ -172,13 +172,11 @@ fn deleteImage(e: *zap.Endpoint, r: zap.Request) void {
     const self = @as(*Self, @fieldParentPtr("endpoint", e));
     std.log.debug("delete image {s}", .{r.query.?});
     r.parseQuery();
-    const path = Util.pathRest(e.settings.path.len, r.getParamSlice("path") orelse return r.setStatus(.bad_request)) orelse return r.setStatus(.bad_request);
-    const size = std.base64.standard.Decoder.calcSizeForSlice(path) catch return r.setStatus(.bad_request);
-    const dest = self.alloc.alloc(u8, size) catch unreachable;
-    std.base64.standard.Decoder.decode(dest, path) catch r.setStatus(.bad_request);
-    std.log.debug("dest: {s}", .{dest});
-    self.image_dir.deleteFile(dest) catch |err| {
-        std.log.debug("Failed to delete file `{s}`: {}", .{dest, err});
+    var path = Util.pathRest(e.settings.path.len, r.getParamSlice("path") orelse return r.setStatus(.bad_request)) orelse return r.setStatus(.bad_request);
+    path = std.Uri.percentDecodeInPlace(@constCast(path));
+
+    self.image_dir.deleteFile(path) catch |err| {
+        std.log.debug("Failed to delete file `{s}`: {}", .{path, err});
         return r.setStatus(.bad_request);
     };
     r.markAsFinished(true);
