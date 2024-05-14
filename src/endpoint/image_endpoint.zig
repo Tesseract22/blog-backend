2c43c98e818660204fa31f6c91fd8945df185ba6
Tesseract22 <caleb2222@qq.com>
2024-05-13 19:08:06 -0500

    rework image endpoint WIP

--- src/endpoint/image_endpoint.zig
+++ src/endpoint/image_endpoint.zig
@@ -27,39 +27,63 @@ pub fn init(
     };
 }
 
-const SaveImageError = error{UnsupportedFormat};
+const SaveImageError = error{UnsupportedFormat, FileAccessError};
 // TODO: Error handling
-fn SaveImage(self: Self, filename: []const u8, data: []const u8) SaveImageError![ImageFolder.len + 10 + 5]u8 {
-    const time_stamp = std.time.microTimestamp();
-    const first_half: u32 = @intCast(time_stamp & 0x00000000ffffffff);
-    const second_half: u32 = @intCast(time_stamp >> 32);
-    var it = std.mem.splitBackwardsScalar(u8, filename, '.');
-    const ext = it.first();
-    if (ext.len > 4) {
-        std.log.err("does not support ext ({s}) len > 4", .{ext});
-        return error.UnsupportedFormat;
-    }
+// fn SaveImage(self: Self, id: usize, filename: []const u8, data: []const u8) SaveImageError![ImageFolder.len + 10 + 5]u8 {
+//     const time_stamp = std.time.microTimestamp();
+//     const first_half: u32 = @intCast(time_stamp & 0x00000000ffffffff);
+//     const second_half: u32 = @intCast(time_stamp >> 32);
+//     var it = std.mem.splitBackwardsScalar(u8, filename, '.');
+//     const ext = it.first();
+//     if (ext.len > 4) {
+//         std.log.err("does not support ext ({s}) len > 4", .{ext});
+//         return error.UnsupportedFormat;
+//     }
 
-    var hash = std.hash.XxHash32.hash(0, filename);
-    hash ^= std.hash.uint32(first_half);
-    hash ^= std.hash.uint32(second_half);
-    hash ^= std.hash.uint32(self.id);
+//     var hash = std.hash.XxHash32.hash(0, filename);
+//     hash ^= std.hash.uint32(first_half);
+//     hash ^= std.hash.uint32(second_half);
+//     hash ^= std.hash.uint32(self.id);
 
-    var name_buf: [PublicFolder.len + ImageFolder.len + 10 + 5]u8 = undefined;
-    @memset(&name_buf, 0);
-    const hash_path = std.fmt.bufPrint(&name_buf, PublicFolder ++ ImageFolder ++ "{}.{s}", .{ hash, ext }) catch unreachable;
-    const f = std.fs.cwd().createFile(hash_path, .{ .read = true, .truncate = true }) catch unreachable;
-    f.writeAll(data) catch unreachable;
-    return name_buf[PublicFolder.len..].*;
+//     var name_buf: [PublicFolder.len + ImageFolder.len + 10 + 5]u8 = undefined;
+//     @memset(&name_buf, 0);
+//     const hash_path = std.fmt.bufPrint(&name_buf, PublicFolder ++ ImageFolder ++ "{}.{s}", .{ hash, ext }) catch unreachable;
+//     const f = std.fs.cwd().createFile(hash_path, .{ .read = true, .truncate = true }) catch unreachable;
+//     f.writeAll(data) catch unreachable;
+//     return name_buf[PublicFolder.len..].*;
+// }
+fn SaveImage(self: Self, id: usize, filename: []const u8, data: []const u8) SaveImageError!void {
+    _ = self; // autofix
+    const cwd = std.fs.cwd();
+    var buf = [_]u8 {0} ** 20;
+    const id_buf = std.fmt.bufPrint(&buf, "{}", .{id}) catch unreachable;
+    cwd.makeDir(id_buf) catch |e| switch (e) {
+        std.posix.MakeDirError.PathAlreadyExists => {},
+        else => return SaveImageError.FileAccessError,
+    };
+    var article_dir = cwd.openDir(id_buf, .{}) catch return SaveImageError.FileAccessError;
+    defer article_dir.close();
+    var f = article_dir.createFile(filename, .{}) catch return SaveImageError.FileAccessError;
+    defer f.close();
+    _ = f.write(data) catch return SaveImageError.FileAccessError;
+}
+fn listImage(self: *Self) void {
+    _ = self; // autofix
+    
 }
 
 pub fn getEndpoint(self: *Self) *zap.Endpoint {
     return &self.endpoint;
 }
+fn postIdFromPath(self: *Self, path: []const u8) ?usize {
+    return idFromPath(self.endpoint.settings.path.len, path);
+}
 
+/// POST  /image/<i>
 fn postImage(e: *zap.Endpoint, r: zap.Request) void {
     const self = @as(*Self, @fieldParentPtr("endpoint", e));
-
+    const path = r.path orelse return r.setStatus(.bad_request);
+    const id = self.postIdFromPath(path) orelse return r.setStatus(.bad_request);
     r.parseBody() catch |err| {
         std.log.err("Parse Body error: {any}. Expected if body is empty", .{err});
     };
@@ -74,10 +98,9 @@ fn postImage(e: *zap.Endpoint, r: zap.Request) void {
                 zap.Request.HttpParam.Hash_Binfile => |*file| {
                     const filename = file.filename orelse "(no filename)";
                     const data = file.data orelse "";
-                    const hash_path = self.SaveImage(filename, data) catch return;
-                    r.sendBody(&hash_path) catch return;
-
-                    // std.log.debug("    contents: {any}\n", .{data});
+                    self.SaveImage(id, filename, data) catch return r.setStatus(.internal_server_error);
+                    
+                    // std.log.debug("    contents: {any}\n", .{data}); 
                 },
                 // multi-file upload
                 zap.Request.HttpParam.Array_Binfile => |*files| {
@@ -88,7 +111,7 @@ fn postImage(e: *zap.Endpoint, r: zap.Request) void {
                     //     // std.log.debug("    contents: {any}\n", .{data});
                     // }
                     std.log.err("Multiple files upload unsupported", .{});
-                    r.sendBody("Multiple files upload unsupported") catch return;
+                    r.sendBody("Multiple files upload unsupported") catch return r.setStatus(.internal_server_error);
                     files.*.deinit();
                 },
                 else => {
@@ -108,12 +131,27 @@ fn postImage(e: *zap.Endpoint, r: zap.Request) void {
 }
 
 fn getImage(e: *zap.Endpoint, r: zap.Request) void {
-    _ = e;
+    const self = @as(*Self, @fieldParentPtr("endpoint", e));
     std.log.debug("getImage", .{});
-    const path = r.path orelse return r.setStatus(.not_found);
-    const ext = std.fs.path.extension(path);
-    const name = path[0 .. path.len - ext.len];
-    std.log.info("{s} {s}", .{ name, ext });
-    if (!std.mem.eql(u8, ext, ".webp")) return r.setStatus(.not_found);
-    return;
+    const path = r.path orelse return r.setStatus(.bad_request);
+    const id = self.postIdFromPath(path) orelse return r.setStatus(.bad_request);
+    var buf = [_]u8 {0} ** 20;
+    const id_buf = std.fmt.bufPrint(&buf, "{}", .{id}) catch unreachable;
+    var dir = std.fs.cwd().openDir(id_buf, .{.iterate = true}) catch |err| switch (err) {
+        error.FileNotFound => 
+            return r.sendJson("[]") catch r.setStatus(.internal_server_error),
+        else => return r.setStatus(.internal_server_error),
+    };
+    defer dir.close();
+    var list = std.ArrayList([]const u8).init(self.alloc);
+    defer list.deinit();
+    var it = dir.iterate();
+    while (it.next() catch return r.setStatus(.internal_server_error)) |entry| {
+        list.append(self.alloc.dupe(u8, entry.name) catch unreachable) catch r.setStatus(.internal_server_error);
+    }
+    const json = std.json.stringifyAlloc(self.alloc, list.items, .{}) catch return r.setStatus(.internal_server_error);
+    r.sendJson(json) catch r.setStatus(.internal_server_error);
+
+    
+    
 }
