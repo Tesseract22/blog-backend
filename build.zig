87d2909373aaf481184d8b09f670d2d796f7e2aa
Tesseract22 <tesseractcat@proton.me>
2025-07-14 09:45:49 +0800

    [FEAT] Make domain an external config
    The domain name being part of the source code does
    not make much sense. Now the build script can genertate a domain file,
    and the src code can @embedFile it.

--- build.zig
+++ build.zig
@@ -3,9 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const pass = b.option([]const u8, "pass", "Generating Password for admin");
-    if (pass) |password| {
-        const hash = std.hash_map.hashString(password);
+    const password = b.option([]const u8, "password", "Generating Password for admin");
+    const domain = b.option([]const u8, "domain", "Domain for the website, default to localhost in debug mode");
+
+    if (password) |password_value| {
+        const hash = std.hash_map.hashString(password_value);
         const len = @typeInfo(@TypeOf(hash)).int.bits / 4;
         var buf: [len]u8 = undefined;
         _ = std.fmt.bufPrint(&buf, "{x:0>16}", .{hash}) catch unreachable;
@@ -13,6 +15,13 @@ pub fn build(b: *std.Build) !void {
         defer auth_file.close();
         _ = try auth_file.writeAll(&buf);
     }
+    if (domain) |domain_value| {
+        var file = try std.fs.cwd().createFile(b.pathFromRoot("src/domain"), .{});
+        defer file.close();
+        _ = try file.writeAll(domain_value);
+    }
+    //const config_opts = b.addOptions();
+    //config_opts.addOption([]const u8, "domain", domain);
 
     const exe = b.addExecutable(.{
         .name = "backend",
@@ -20,6 +29,7 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
+    //exe.root_module.addOptions("meta_config", config_opts);
 
     // zap
     const zap = b.dependency("zap", .{
